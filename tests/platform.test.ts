e2abeec5ca3d80883829464d8e36c8e34cc28eef
jarvisjiang <jarvisjiang@tencent.com>
2024-05-15 14:59:57 +0000

    add test cases

--- tests/platform.test.ts
+++ tests/platform.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@jest/globals';
-import { getTargetType } from '../src/mod.ts';
+import { getTargetType, isDevtools, isWx } from '../src/mod.ts';
 
 test('getTargetType is web', () => {
     expect(getTargetType()).toBe('web');
+    expect(isWx()).toBe(false);
+    expect(isDevtools()).toBe(false);
 });
