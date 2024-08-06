1b027e0b8bc49fbbab06fc174107a75d9c7c7054
jarvisjiang <jarvisjiang@tencent.com>
2024-08-06 02:03:21 +0000

    export some util methods

--- src/std/utils/mod.ts
+++ src/std/utils/mod.ts
@@ -1,4 +1,15 @@
-import { Err, type IOResult } from 'happy-rusty';
+import { Err, Ok, type AsyncIOResult, type IOResult } from 'happy-rusty';
+
+/**
+ * 将小游戏错误对象转换为 `Error` 类型。
+ * @param err - 小游戏错误对象。
+ * @returns 转换后的 `Error` 对象。
+ */
+export function minaErrorToError(err: {
+    errMsg: string;
+}): Error {
+    return new Error(err.errMsg);
+}
 
 /**
  * 将错误对象转换为 IOResult 类型。
@@ -7,5 +18,57 @@ import { Err, type IOResult } from 'happy-rusty';
  * @returns 转换后的 IOResult 对象。
  */
 export function generalErrorToResult<T>(err: WechatMinigame.GeneralCallbackResult): IOResult<T> {
-    return Err(new Error(err.errMsg));
+    return Err(minaErrorToError(err));
+}
+
+/**
+ * 执行同步函数，预期异常都是 `WechatMinigame.GeneralCallbackResult`。
+ * @param op - 需要执行的同步函数。
+ * @returns IOResult。
+ */
+export function tryGeneralSyncOp<T>(op: () => T): IOResult<T> {
+    try {
+        return Ok(op());
+    } catch (e) {
+        return generalErrorToResult(e as WechatMinigame.GeneralCallbackResult);
+    }
+}
+
+/**
+ * 执行异步函数，预期异常都是 `WechatMinigame.GeneralCallbackResult`。
+ * @param op - 需要执行的异步函数。
+ * @returns AsyncIOResult。
+ */
+export async function tryGeneralAsyncOp<T>(op: () => Promise<T>): AsyncIOResult<T> {
+    try {
+        return Ok(await op());
+    } catch (e) {
+        return generalErrorToResult(e as WechatMinigame.GeneralCallbackResult);
+    }
+}
+
+/**
+ * 执行同步函数，预期异常都是 `DOMException`。
+ * @param op - 需要执行的同步函数。
+ * @returns IOResult。
+ */
+export function tryDOMSyncOp<T>(op: () => T): IOResult<T> {
+    try {
+        return Ok(op());
+    } catch (e) {
+        return Err(e as DOMException);
+    }
+}
+
+/**
+ * 执行异步函数，预期异常都是 `DOMException`。
+ * @param op - 需要执行的异步函数。
+ * @returns AsyncIOResult。
+ */
+export async function tryDOMAsyncOp<T>(op: () => Promise<T>): AsyncIOResult<T> {
+    try {
+        return Ok(await op());
+    } catch (e) {
+        return Err(e as DOMException);
+    }
 }
