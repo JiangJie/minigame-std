095f429e88026bb33497937fbf864a7c99740c82
jarvisjiang <jarvisjiang@tencent.com>
2024-08-14 17:46:28 +0800

    support calculate `sha1`

--- src/mod.ts
+++ src/mod.ts
@@ -6,7 +6,6 @@ export * as cryptos /* Avoid conflict with global `crypto` */ from './std/crypto
 export * from './std/event/mod.ts';
 export * from './std/fetch/mod.ts';
 export * as fs from './std/fs/mod.ts';
-export * as hash from './std/hash/mod.ts';
 export * from './std/network/mod.ts';
 export * as platform from './std/platform/mod.ts';
 export * from './std/socket/mod.ts';
--- src/std/crypto/mod.ts
+++ src/std/crypto/mod.ts
@@ -1,1 +1,32 @@
+import { isMinaEnv } from '../../macros/env.ts';
+import { textEncode } from '../codec/mod.ts';
+import { hexFromBuffer } from '../utils/mod.ts';
+import { Md5 } from './md5.ts';
+import { Sha1 } from './sha1.ts';
+
+export { Md5 } from './md5.ts';
 export * as rsa from './rsa.ts';
+
+/**
+ * 计算字符串或者 buffer 的 MD5 值，结果用16进制字符串表示。
+ * @param data - 需要计算 MD5 值的数据。
+ * @returns 计算得到的 MD5 十六进制字符串。
+ */
+export function md5(data: string | BufferSource): string {
+    return new Md5().update(data).toString();
+}
+
+/**
+ * 计算字符串的 SHA-1 哈希值，结果用16进制字符串表示。
+ * @param data - 需要计算 SHA-1 哈希值的数据。
+ * @returns 计算得到的 SHA-1 十六进制字符串。
+ */
+export async function sha1(data: string): Promise<string> {
+    if (isMinaEnv()) {
+        return Promise.resolve(new Sha1().update(data).toString());
+    }
+
+    const encodedData = textEncode(data);
+    const hashBuffer = await crypto.subtle.digest('SHA-1', encodedData);
+    return hexFromBuffer(hashBuffer);
+}
