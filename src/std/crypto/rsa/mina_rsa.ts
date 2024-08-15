29f262a8d503080c793780e6e0ddd26309a5e040
jarvisjiang <jarvisjiang@tencent.com>
2024-08-15 17:03:53 +0800

    `publicKeyFromPem` support `hash` parameter

--- src/std/crypto/rsa/mina_rsa.ts
+++ src/std/crypto/rsa/mina_rsa.ts
@@ -1,13 +1,25 @@
 import forge from 'node-forge';
-import { encodeBase64 } from '../../base64/mod.ts';
-import type { RSAPublicKey } from './rsa_defines.ts';
+import type { RSAPublicKey, SHA } from './rsa_defines.ts';
 
-export function publicKeyFromPem(pem: string): RSAPublicKey {
+/**
+ * Import a public key from a PEM encoded string for encryption.
+ * @param pem - PEM encoded string.
+ * @param hash - Hash algorithm.
+ * @returns
+ */
+export function publicKeyFromPem(pem: string, hash: SHA): RSAPublicKey {
     const publicKey = forge.pki.publicKeyFromPem(pem);
 
     return {
         encrypt(data: string): Promise<string> {
-            return Promise.resolve(encodeBase64(publicKey.encrypt(data, 'RSA-OAEP')));
+            // eg: SHA-1 => sha1
+            const md = hash.toLowerCase().replace('-', '');
+            const encryptedData = publicKey.encrypt(data, 'RSA-OAEP', {
+                // bypassing ts errors
+                md: forge.md[md as 'sha1'].create(),
+            });
+
+            return Promise.resolve(forge.util.encode64(encryptedData));
         },
     };
 }
--- src/std/crypto/rsa/mod.ts
+++ src/std/crypto/rsa/mod.ts
@@ -5,8 +5,14 @@ import { publicKeyFromPem as webPublicKeyFromPem } from './web_rsa.ts';
 
 export * from './rsa_defines.ts';
 
+/**
+ * Import a public key from a PEM encoded string for encryption.
+ * @param pem - PEM encoded string.
+ * @param hash - Hash algorithm.
+ * @returns
+ */
 export function publicKeyFromPem(pem: string, hash: SHA): Promise<RSAPublicKey> {
     return isMinaEnv()
-        ? Promise.resolve(minaPublicKeyFromPem(pem))
+        ? Promise.resolve(minaPublicKeyFromPem(pem, hash))
         : webPublicKeyFromPem(pem, hash);
 }
