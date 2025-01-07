bcdc9bf32b1c71ac99a04554d0b272579906788f
jarvisjiang <jarvisjiang@tencent.com>
2025-01-07 02:38:25 +0000

    fix: `wx.getDeviceInfo` may not exist

--- src/std/platform/device.ts
+++ src/std/platform/device.ts
@@ -5,6 +5,8 @@ let deviceInfo: WechatMinigame.DeviceInfo;
  * @returns 返回小游戏的设备信息对象。
  */
 export function getDeviceInfo(): WechatMinigame.DeviceInfo {
-    deviceInfo ??= wx.getDeviceInfo();
+    // 兼容基础库低版本
+    // TODO 暂时只用了platform属性，可安全强转类型
+    deviceInfo ??= wx.getDeviceInfo ? wx.getDeviceInfo() : (wx.getSystemInfoSync() as unknown as WechatMinigame.DeviceInfo);
     return deviceInfo;
 }
