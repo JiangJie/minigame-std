f7b95c7fd65a20bc1c332107c699185ba5e0519c
jarvisjiang <jarvisjiang@tencent.com>
2026-01-19 20:04:32 +0800

    refactor: remove main.ts entry point and clean up game.js
    - Remove deprecated main.ts entry point
    - Remove debug logs from game.js
    - Add URL polyfill for minigame environment

--- packages/minigame-test/game.js
+++ packages/minigame-test/game.js
@@ -1,6 +1,4 @@
+GameGlobal.URL = class { };
 GameGlobal.URLSearchParams = class { };
-
-console.log(`wx.env.USER_DATA_PATH = ${ wx.env.USER_DATA_PATH }`);
-console.log('wx.getAccountInfoSync()', wx.getAccountInfoSync());
 
 require('./dist/index');
